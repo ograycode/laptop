ba61cfb4affc56fd141c22a93b3032710dc92aee
ograycode <jason.alan.gray@gmail.com>
2013-09-26 13:39:59 -0400

    Changed from move to copy of the site.pp file

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,5 @@ mv puppetlabs-apt-1.3.0 /etc/puppet/modules/apt
 wget -nc http://forge.puppetlabs.com/puppetlabs/stdlib/4.1.0.tar.gz
 tar -zxvf 4.1.0.tar.gz
 mv puppetlabs-stdlib-4.1.0 /etc/puppet/modules/stdlib
-mv site.pp /etc/puppet/manifests/site.pp
+cp site.pp /etc/puppet/manifests/site.pp
 puppet apply /etc/puppet/manifests/site.pp
