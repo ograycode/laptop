5e66ec8b3d3fe08be03736064b6052a843b323da
ograycode <jason.alan.gray@gmail.com>
2013-09-27 13:47:12 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
+#Install git and clone the repo
 apt-get update
 apt-get -y install git
 git clone https://github.com/ograycode/laptop.git
 cd laptop/
+#Install puppet master and the modules needed
 apt-get -y install puppetmaster
 wget -nc http://forge.puppetlabs.com/puppetlabs/apt/1.3.0.tar.gz
 tar -zxvf 1.3.0.tar.gz
@@ -9,7 +11,9 @@ mv puppetlabs-apt-1.3.0 /etc/puppet/modules/apt
 wget -nc http://forge.puppetlabs.com/puppetlabs/stdlib/4.1.0.tar.gz
 tar -zxvf 4.1.0.tar.gz
 mv puppetlabs-stdlib-4.1.0 /etc/puppet/modules/stdlib
+#copy the puppet manifest and apply it
 cp site.pp /etc/puppet/manifests/site.pp
 puppet apply /etc/puppet/manifests/site.pp
+#prepare the dock/plank
 rm -r ~/.config/plank/dock1
 cp -a files/dock1 ~/.config/plank
