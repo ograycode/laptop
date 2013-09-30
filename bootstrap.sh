c76407a8e95a77ca6d45d47a5d30f2d1a21729a9
ograycode <jason.alan.gray@gmail.com>
2013-09-30 17:05:55 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,13 +4,8 @@ sudo apt-get update
 
 echo "---------------------------------------------------------"
 echo "Installing git..."
-sudo apt-get -y install git
-
-#prepare the dock/plank
 echo "---------------------------------------------------------"
-echo "Setting up plank..."
-sudo rm -r ~/.config/plank/dock1
-sudo cp -a files/dock1 ~/.config/plank
+sudo apt-get -y install git
 
 #clone the repo
 echo "---------------------------------------------------------"
@@ -18,9 +13,17 @@ echo "Cloning the repo..."
 git clone https://github.com/ograycode/laptop.git
 cd laptop/
 
+#prepare the dock/plank
+echo "---------------------------------------------------------"
+echo "Setting up plank..."
+echo "---------------------------------------------------------"
+sudo rm -r ~/.config/plank/dock1
+sudo cp -a files/dock1 ~/.config/plank
+
 #Install puppet master and the modules needed
 echo "---------------------------------------------------------"
 echo "Installing puppet and the needed modules..."
+echo "---------------------------------------------------------"
 sudo apt-get -y install puppetmaster
 wget -nc http://forge.puppetlabs.com/puppetlabs/apt/1.3.0.tar.gz
 tar -zxvf 1.3.0.tar.gz
@@ -32,11 +35,13 @@ sudo mv puppetlabs-stdlib-4.1.0 /etc/puppet/modules/stdlib
 #copy the puppet manifest and apply it
 echo "---------------------------------------------------------"
 echo "Applying the puppet manifest..."
+echo "---------------------------------------------------------"
 sudo cp site.pp /etc/puppet/manifests/site.pp
 sudo puppet apply /etc/puppet/manifests/site.pp
 
 echo "---------------------------------------------------------"
 echo "Setting the wallpaper..."
+echo "---------------------------------------------------------"
 gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/ARTcast.jpg
 
 echo "---------------------------------------------------------"
