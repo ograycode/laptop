65ee61cfba8516c80149e7a58f318f6cb15a379f
ograycode <jason.alan.gray@gmail.com>
2013-09-30 12:48:03 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,20 @@
 #Install git and clone the repo
-apt-get update
-apt-get -y install git
+sudo apt-get update
+sudo apt-get -y install git
 git clone https://github.com/ograycode/laptop.git
 cd laptop/
 #Install puppet master and the modules needed
-apt-get -y install puppetmaster
+sudo apt-get -y install puppetmaster
 wget -nc http://forge.puppetlabs.com/puppetlabs/apt/1.3.0.tar.gz
 tar -zxvf 1.3.0.tar.gz
-mv puppetlabs-apt-1.3.0 /etc/puppet/modules/apt
+sudo mv puppetlabs-apt-1.3.0 /etc/puppet/modules/apt
 wget -nc http://forge.puppetlabs.com/puppetlabs/stdlib/4.1.0.tar.gz
 tar -zxvf 4.1.0.tar.gz
-mv puppetlabs-stdlib-4.1.0 /etc/puppet/modules/stdlib
+sudo mv puppetlabs-stdlib-4.1.0 /etc/puppet/modules/stdlib
 #copy the puppet manifest and apply it
-cp site.pp /etc/puppet/manifests/site.pp
-puppet apply /etc/puppet/manifests/site.pp
+sudo cp site.pp /etc/puppet/manifests/site.pp
+sudo puppet apply /etc/puppet/manifests/site.pp
 #prepare the dock/plank
-rm -r ~/.config/plank/dock1
-cp -a files/dock1 ~/.config/plank
+sudo rm -r ~/.config/plank/dock1
+sudo cp -a files/dock1 ~/.config/plank
+gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/ARTcast.jpg
