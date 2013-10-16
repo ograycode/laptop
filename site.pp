ad5590500dff24d7de7dd940ae0efb617ae4cc18
ograycode <jason.alan.gray@gmail.com>
2013-10-15 20:19:02 -0400

    Update site.pp

--- site.pp
+++ site.pp
@@ -49,7 +49,6 @@ apt::ppa {'ppa:ehoover/compholio':
 package {'netflix-desktop':}
 
 exec { "accept-msttcorefonts-license":
-  require => 'ubuntu-restricted-extras',
   command => "/bin/sh -c \"echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections\""
 }
 
